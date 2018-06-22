3b25c17cd6913d932a6ca31bed7ec108e37650bf
Victor Avila <victor.avila@venturplex.com>
2018-06-22 14:40:03 -0700

    add necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
-import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AppMaterialModule } from './app-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AppMaterialModule } from './app-material.module';
+
+import { SpotifyService } from './shared/spotify.service';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    AppMaterialModule
+  ],
   declarations: [
     AppComponent,
     SearchComponent,
     LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AppMaterialModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
+  providers: [SpotifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
