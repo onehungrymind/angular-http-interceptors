327cacb11624f09a0a9c061f518b223358acfd5d
Victor Avila <victor.avila@venturplex.com>
2018-06-25 13:37:36 -0700

    misc

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,8 +14,9 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    const redirect = `${window.location.protocol}//${window.location.host}/search`;
     (<any>window).location =
-      `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=http://localhost:4200/search`;
+      `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirect}`;
   }
 
 }
